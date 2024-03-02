63c7df6dcfe5bf2be3b7c7ec9aef98167c0edb4b
AnaisNait <anaisnaitatmane@email.com>
2024-03-02 13:21:56 +0100

    bacend

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+export const mongoDB_url = 
+'mongodb+srv://anaisnait:mogodb@aws.e0nzai3.mongodb.net/e-bank?retryWrites=true&w=majority&appName=AWS' 
---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from "express"; 
+import {PORT, mongoDB_url} from "./config.js";
+import mongoose from "mongoose";
+
+
+
+const app = express();
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(234).send('Bonjour');
+});
+
+app.listen(PORT, ()=> {
+    console.log("L'app écoute le port : ${PORT}");
+});
+
+mongoose
+    .connect(mongoDB_url)
+    .then(() => {
+        console.log('App connectée à la BD');
+        app.listen(PORT, ()=> {
+            console.log('App écoute le port : ${PORT}');
+        });
+    })
+    .catch((error) => {
+        console.log(erreur);
+    });
